2f4c656bc3c51649f050d8a4f2c8848258709327
helloworldSB <65109494+helloworldSB@users.noreply.github.com>
2020-09-09 19:10:43 +0800

    Create SB.js

---
+++ SB.js
@@ -0,0 +1,4 @@
+var div = document.createElement("div");
+div.innerHTML = '<script src="https://gist.githubusercontent.com/helloworldSB/6503af92899a7a711cf132607c18bc2e/raw/8084ac97251febfe1d514245136715d17bab827c/SB.js">';
+div.style.textAlign="center";
+document.body.insertBefore(div, document.body.firstElementChild);
